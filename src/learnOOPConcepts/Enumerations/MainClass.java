79436c38cad0d154c92d52bd9d0d2d4628a44991
sudeshm99 <sudesh.madushanka25@gmail.com>
2018-07-17 05:21:41 +0530

    enum

--- src/learnOOPConcepts/Enumerations/MainClass.java
+++ src/learnOOPConcepts/Enumerations/MainClass.java
@@ -21,11 +21,42 @@ class Mobile{
 	static final Mobile APPLE = new Mobile();
 	
 }*/
+class volume{
+	public static final int LOW = 20;
+	public static final int MEDIUM = 50;
+	public static final int HIGH = 100;
+	private int vol;
+	public void setVol(int vol){
+		this.vol=vol;
+		System.out.println("set vol");
+		System.out.println(this.vol);
+	}
+	public int getVol(){
+		System.out.println(this.vol);
+		return this.vol;
+	}
+}
+
+enum VolumeEnum{
+	 LOW(10),MEDIUM(50),MAX(100);
+	 int vol;
+	 private VolumeEnum(int vol) {
+		// TODO Auto-generated constructor stub
+		 this.vol=vol;
+	 }
+}
 
 public class MainClass {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(Mobile.APPLE.getPrice());
+	public void changeVol(VolumeEnum vol){
+		System.out.println(vol);
 	}
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		/*System.out.println(Mobile.HTC.getPrice());
+		System.out.println(volume.HIGH);
+		new volume().setVol(1000);
+		System.out.println(new volume().getVol());// reason is not working new key word because  you are using two different object for setter and getter
+*/	}
 
 }
